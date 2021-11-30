8734c21b09fd525b95150d1123b5d97cf6901292
Yuyi <yuyi365@gmail.com>
2021-11-30 16:59:31 -0500

    commit tree 727dd9416cc5ffb8a267cf17aab7e86446b9c797

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,26 @@
 class Song
-
   attr_accessor :name, :album, :id
 
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    # insert the song
+    DB[:conn].execute(sql, self.name, self.album)
+
+    # get the song ID from the database and save it to the Ruby instance
+    self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM songs')[0][0]
+
+    # return the Ruby instance
+    self
   end
 
   def self.drop_table
@@ -28,20 +43,12 @@ class Song
     DB[:conn].execute(sql)
   end
 
-  def save
+  def self.drop_table
     sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
+      DROP TABLE songs;
     SQL
 
-    # insert the song
-    DB[:conn].execute(sql, self.name, self.album)
-
-    # get the song ID from the database and save it to the Ruby instance
-    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
-    # return the Ruby instance
-    self
+    DB[:conn].execute(sql)
   end
 
   def self.create(name:, album:)
@@ -49,4 +56,28 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+  end
 end
